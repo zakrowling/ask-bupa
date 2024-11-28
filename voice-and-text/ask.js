4cfa037432e46f466801a598d06959d936b39699
Zak Rowling <zakrowling@gmail.com>
2024-11-28 13:40:45 +1000

    Update ask.js

--- voice-and-text/ask.js
+++ voice-and-text/ask.js
@@ -6,6 +6,7 @@ recognition.lang = 'en-US';
 
 recognition.onstart = () => {
     startButton.classList.add("active");
+    outputDiv.classList.add("sound-bars");
     startButton.textContent = 'Listening...';
     outputDiv.textContent = '';
 };
@@ -122,6 +123,7 @@ recognition.onresult = (event) => {
 
 recognition.onend = () => {
     startButton.classList.remove("active");
+    outputDiv.classList.remove("sound-bars");
     startButton.textContent = 'Ask Bupa';
 };
 
